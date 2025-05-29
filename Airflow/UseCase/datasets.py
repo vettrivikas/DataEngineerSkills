20e1481834462b6618749f68fbaf5ef5f5d36460
vettrivikas <32515723+vettrivikas@users.noreply.github.com>
2025-05-29 18:19:00 +0530

    Update datasets.py

--- Airflow/UseCase/datasets.py
+++ Airflow/UseCase/datasets.py
@@ -6,3 +6,4 @@ level_1_job_3_dataset = Dataset("ds://level_1_job_3")
 level_1_job_4_dataset = Dataset("ds://level_1_job_4")
 
 level_3_jobs_2_dataset = Dataset("ds://level_3_jobs_2")  # intermediate dataset
+
