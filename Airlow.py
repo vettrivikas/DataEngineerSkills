672d9faa174e7ef54480dd0739b9ef643db443f2
vettrivikas <32515723+vettrivikas@users.noreply.github.com>
2025-09-16 22:02:51 +0530

    Update Airlow.py

--- Airlow.py
+++ Airlow.py
@@ -1,14 +1,11 @@
 def format_message_for_email(message: dict) -> str:
     return (
         "{\n"
-        f"Event ID      : {message.get('event_id')}\n"
-        f"Event         : {message.get('event')}\n"
-        f"Process Name  : {message.get('process_name')}\n"
-        f"Data Date     : {message.get('data_date')}\n"
-        f"Completed At  : {message.get('process_com_dt')}\n"
-        f"Audit ID      : {message.get('audit_id')}\n"
+        f"'Event ID'     : '{message.get('event_id')}'\n"
+        f"'Event'        : '{message.get('event')}'\n"
+        f"'Process Name' : '{message.get('process_name')}'\n"
+        f"'Data Date'    : '{message.get('data_date')}'\n"
+        f"'Completed At' : '{message.get('process_com_dt')}'\n"
+        f"'Audit ID'     : '{message.get('audit_id')}'\n"
         "}"
     )
-
-email_body = format_message_for_email(message)
-print(email_body)
