7e65f452fbbf99da735e1041cd49fc05b0ce8a0a
vettrivikas <32515723+vettrivikas@users.noreply.github.com>
2025-10-17 15:27:48 +0530

    Update Airlow.py

--- Airlow.py
+++ Airlow.py
@@ -1,12 +1,18 @@
-def format_message_for_email(message: dict) -> str:
-    return (
-        "{\n"
-        f"'event_id'       : '{message.get('event_id')}',\n"
-        f"'event'          : '{message.get('event')}',\n"
-        f"'database'       : '{message.get('database', '')}',\n"
-        f"'process_name'   : '{message.get('process_name')}',\n"
-        f"'data_date'      : '{message.get('data_date')}',\n"
-        f"'process_com_dt' : '{message.get('process_com_dt')}',\n"
-        f"'audit_id'       : '{message.get('audit_id')}',\n"
-        "}"
-    )
+Subject: TOCC Extract Files Ready for Testing
+
+Hi Team,
+
+I have generated the required files for your testing. The Glue jobs were executed to create the respective extract files, and I have renamed them accordingly.
+
+The following files are available:
+
+TOCC Parquet change extract files (for all three)
+
+TOCC current production version script extract files (for all three)
+
+TOCC old script extract files (for all three)
+
+I’ve also updated the latest file names in the Excel sheet — please refer to it for the correct file references.
+All extracts have been validated from my end. Kindly proceed with testing from your side.
+
+Best regards,
