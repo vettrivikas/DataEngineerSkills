a9ef45f32c775ee101e014ca619a2e887e47de8c
vettrivikas <32515723+vettrivikas@users.noreply.github.com>
2025-10-17 16:07:51 +0530

    Update Airlow.py

--- Airlow.py
+++ Airlow.py
@@ -1,18 +1,1 @@
-Subject: TOCC Extract Files Ready for Testing
-
-Hi Team,
-
-I have generated the required files for your testing. The Glue jobs were executed to create the respective extract files, and I have renamed them accordingly.
-
-The following files are available:
-
-TOCC Parquet change extract files (for all three)
-
-TOCC current production version script extract files (for all three)
-
-TOCC old script extract files (for all three)
-
-I’ve also updated the latest file names in the Excel sheet — please refer to it for the correct file references.
-All extracts have been validated from my end. Kindly proceed with testing from your side.
-
-Best regards,
+airflow.py
