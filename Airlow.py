963c45fe0821579d0ee5ab6438de35b32af35940
vettrivikas <32515723+vettrivikas@users.noreply.github.com>
2026-02-17 17:23:09 +0530

    Update Airlow.py

--- Airlow.py
+++ Airlow.py
@@ -1,8 +1,1 @@
-Hi Team,
-
-I have debugged the Airflow DAG. The audit update issue is now fixed.
-
-The workflow is triggering successfully, and I can see the Job ID. However, the actual tasks inside the workflow are not starting.
-
-Could you please check with the Cloud team and share the workflow execution screenshots or logs?
-
+.
